7825d4c636b17b2b5e00d09402ab2a712e541619
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-04-17 08:32:42 +0000

    Upd

--- comp_xyzcsp.bat
+++ comp_xyzcsp.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
 cl /I"..\include" /nologo /MT /O2 /c xyzcsp.c
 rc /I"..\include" xyzcsp.rc
 link /SUBSYSTEM:WINDOWS",5.0" /NODEFAULTLIB /DLL /DEF:xyzcsp.def /MACHINE:x86 /OUT:xyzcsp.dll xyzcsp.obj openssl.lib advapi32.lib kernel32.lib msvcrt.lib gdi32.lib user32.lib xyzcsp.res
--- make_lib.bat
+++
@@ -1,2 +0,0 @@
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-lib /out:openssl.lib @flist.txt
